71542ed75f6b5de82bbcad1eea8b4dc2e40d5c28
LucasFrazaoDev <lucas_devfrazao@outlook.com>
2023-08-20 14:34:07 -0300

    Cinemachine and new input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,34 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float m_moveSpeed = 5f;
+    [SerializeField] private GameInput m_gameInput;
     private bool m_isWalking = false;
 
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = m_gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * m_moveSpeed * Time.deltaTime;
 
