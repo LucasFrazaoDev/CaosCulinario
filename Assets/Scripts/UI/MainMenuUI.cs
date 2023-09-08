3c0342176fb8fbd0fd626fd3ae5cc0809b6ab495
LucasFrazaoDev <lucas_devfrazao@outlook.com>
2023-09-08 16:30:35 -0300

    Main menu and loading scenes

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button m_playButton;
+    [SerializeField] private Button m_quitButton;
+
+    private void Awake()
+    {
+        m_playButton.onClick.AddListener(() => { Loader.Load(Loader.Scene.GameScene); });
+        m_playButton.onClick.AddListener(() => { Application.Quit(); });
+    }
+}
